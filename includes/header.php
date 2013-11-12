92d527fb6d15811ff41e72089d70dd0465f12c6e
Yining Karl Li <betajippity@gmail.com>
2013-11-11 23:50:14 -0500

    New retina res favicon, headers are now separated

---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<LINK REL="SHORTCUT ICON" HREF="/includes/favicon.png" />
+		<link rel="apple-touch-icon" href="/includes/icon.png"/> 
+		<link rel='stylesheet' href="/includes/css/style.css" type='text/css' media='screen' />
+		
+		<script type="text/javascript" src="/includes/js/jquery.js"></script>
+		<script type="text/javascript" src="/includes/js/navbar.js"></script>
+		
+		<script type="text/javascript" src="http://use.typekit.com/ivw5chn.js"></script>
+		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+		<script type="text/javascript" src="/includes/js/analytics.js"></script>
--- projects/volumetricrenderer/index.php
+++ projects/volumetricrenderer/index.php
@@ -3,17 +3,10 @@
 	<head>
 		<meta http-equiv='Content-Type' content='application/xhtml+xml; charset=utf-8' />
 		<title>YINING KARL LI | Volumetric Renderer</title>
-		<LINK REL="SHORTCUT ICON" HREF="../../includes/favicon.ico" />
-		<link rel="apple-touch-icon" href="../../includes/icon.png"/> 
-		<link rel='stylesheet' href='../../includes/css/style.css' type='text/css' media='screen' />
+		<?php
+			include '../../includes/header.php';
+		?>
 		<link rel='stylesheet' href='../projects.css' type='text/css' media='screen' />
-
-		<script type="text/javascript" src="../../includes/js/jquery.js"></script>
-		<script type="text/javascript" src="../../includes/js/navbar.js"></script>
-		
-		<script type="text/javascript" src="http://use.typekit.com/ivw5chn.js"></script>
-		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-		<script type="text/javascript" src="../../includes/js/analytics.js"></script>
 	</head>
 	<?php
 		include '../../includes/navigation.php';

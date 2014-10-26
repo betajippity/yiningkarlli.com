a3b0408222e2060784d154266833bf2894f55fe3
Yining Karl Li <betajippity@gmail.com>
2014-10-25 23:42:49 -0400

    Updates to resume

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 		<div class="content">
 				<p><iframe src="http://player.vimeo.com/video/50127144?color=ffffff" width="900" height="508" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 				<br><br>
-				Born in Hannover, Germany and raised in Philadelphia, Pennsylvania, Yining Karl Li is computer graphics researcher and student. Currently, Karl is Master of Science in Computer Graphics candidate at Cornell University's <a href="http://www.graphics.cornell.edu/">Program of Computer Graphics</a>.<p>
+				Born in Hannover, Germany and raised in Philadelphia, Pennsylvania, Yining Karl Li is a computer graphics researcher and student. Currently, Karl is a Master of Science in Computer Graphics candidate at Cornell University's <a href="http://www.graphics.cornell.edu/">Program of Computer Graphics</a>.<p>
 				
 				Previously, Karl did his undergraduate studies at the <a href="http://www.upenn.edu/">University of Pennsylvania</a>.<p>
 				During the summer of 2013, Karl interned at <a href="http://www.pixar.com/">Pixar Animation Studios</a> as part of the <a href="http://graphics.pixar.com/research/">Pixar Research Group</a>.<p>
--- resume/index.php
+++ resume/index.php
@@ -3,10 +3,19 @@
 	<head>
 		<meta http-equiv='Content-Type' content='application/xhtml+xml; charset=utf-8' />
 		<title>YINING KARL LI | Resume</title>
-		<?php
-			include '../includes/header.php';
-		?>
+		<link rel='stylesheet' href="../includes/css/style.css" type='text/css' media='screen' />
 		<link rel='stylesheet' href='resume.css' type='text/css' media='screen' />
+		<link rel="apple-touch-icon" href="../includes/icon.png"/>
+		<LINK REL="SHORTCUT ICON" HREF="../includes/favicon.ico" />
+		<script type="text/javascript" src="http://use.typekit.com/ivw5chn.js"></script>
+		
+		<script type="text/javascript" src="../includes/js/jquery.js"></script>
+		<script type="text/javascript" src="../includes/js/smoothscroll.js"></script>
+		<script type="text/javascript" src="../includes/js/navbar.js"></script>
+		
+		<script type="text/javascript" src="http://use.typekit.com/ivw5chn.js"></script>
+		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+		<script type="text/javascript" src="../includes/js/analytics.js"></script>
 	</head>
 	<?php
 		include '../includes/navigation.php';
@@ -14,7 +23,7 @@
 	<body>
 		<div class="content">
 			<div class="title">Resume</div>
-			<div class="subtitle">(PDF Version Available <a href="http://www.yiningkarlli.com/resume/karlFall2013Resume.pdf">Here</a>)</div>
+			<div class="subtitle">(PDF Version Available <a href="http://www.yiningkarlli.com/resume/karlFall2014Resume.pdf">Here</a>)</div>
 			<br><br>
 			
 			<!-- EDUCATION SECTION -->
@@ -24,9 +33,9 @@
 					<div class="resumeheader">EDUCATION</div>
 				</div>		
 				<div class="resumerightbar">
-					<div class="subtitle"><a href="http://www.cornell.edu/">Cornell University</a> | Ithaca, NY | Sept. 2013 - May 2015</div>
+					<div class="subtitle"><a href="http://www.cornell.edu/">Cornell University</a> | Ithaca, NY | Sep 2013 - May 2015</div>
 					<p>Program of Computer Graphics <b>Master of Science in Computer Graphics</b>, advisor: <a href="http://www.graphics.cornell.edu/people/director.html">Dr. Donald P. Greenberg</a> (Expected Spring 2015)
-					<div class="subtitle"><a href="http://www.upenn.edu/">University of Pennsylvania</a> | Philadelphia, PA | Sept. 2009 - May 2013</div>
+					<div class="subtitle"><a href="http://www.upenn.edu/">University of Pennsylvania</a> | Philadelphia, PA | Sep 2009 - May 2013</div>
 					<p>Wharton School of Business <b>Bachelor of Science in Economics</b>, concentration in Marketing (Completed May 2013)
 				</div>
 			</div>
@@ -38,22 +47,34 @@
 					<div class="resumeheader">WORK EXPERIENCE</div>
 				</div>
 				<div class="resumerightbar">
-					<div class="subtitle"><a href="http://www.pixar.com/">Pixar Animation Studios</a> | Emeryville, CA | June 2013 - Aug. 2013</div>
-					<p><b>Research Group Intern</b> | Working as part of a research team developing an undisclosed rendering related project. (Supervisors: <a href="http://graphics.pixar.com/people/derose/">Tony DeRose</a>, <a href="http://graphics.pixar.com/people/mmeyer/">Mark Meyer</a>)
+					
+					<div class="subtitle"><a href="http://graphics.cornell.edu/">Cornell University Program of Computer Graphics</a> | Ithaca, NY | Sep 2013 - Present</div>
+					<p><b>Research Assistant</b> |  Conducting research into <b>fast physically based rendering</b> and 
+<b>accurate illumination models</b>. | Supervisors: <a href="http://www.graphics.cornell.edu/people/director.html">Donald P. Greenberg</a>, <a href="http://www.graphics.cornell.edu/~kiderj/">Joseph T. Kider</a>
 					<br>
-					<div class="subtitle"><a href="http://www.dreamworksanimation.com/">Dreamworks Animation SKG, Inc.</a> | Glendale, CA | June 2012 - Aug. 2012</div>
-					<p><b>Global Department Technical Director Intern</b> | Responsible for developing tools and technology used by other departments to produce animated films. (Supervisor: Jon Shih)
+					
+					
+					
+					<div class="subtitle"><a href="http://www.pixar.com/">Pixar Animation Studios</a> | Emeryville, CA | Jun 2013 - Aug 2013</div>
+					<p><b>Research Group Intern</b> | Prototyped <b>subsurface scattering</b> and <b>fur rendering</b> components of Pixar's <b>interactive physically based rendering</b> preview system. | Supervisors: <a href="http://graphics.pixar.com/people/derose/">Tony DeRose</a>, <a href="http://graphics.pixar.com/people/mmeyer/">Mark Meyer</a>
 					<br>
-					<div class="subtitle"><a href="http://www.pixar.com/">Pixar Animation Studios</a> | Emeryville, CA | June 2011 - Aug. 2011</div>
-					<p><b>Pixar Undergraduate Program Intern</b> | Completed weekly assignments in all areas of the Pixar production pipeline to gain experience in production animated film creation. (Supervisor: Fran Kalal)
+					<div class="subtitle"><a href="http://cg.cis.upenn.edu/">University of Pennsylvania</a> | Philadelphia, PA | Jan 2012 - May 2013</div>
+					<p><b>Physically Based Animation (Spring 2013) TA</b> | Developed assignments,
+held office hours, graded assignments, etc. | Supervisor: <a href="http://www.seas.upenn.edu/~ladislav/">Ladislav Kavan</a>
 					<br>
-					<div class="subtitle"><a href="http://cg.cis.upenn.edu/">University of Pennsylvania</a> | Philadelphia, PA | Jan. 2012 - May. 2013</div>
-					<p><b>CIS277 Teaching Assistant (Spring 2012)</b> | Held office hours, assisted students, and graded assignments for the undergraduate level course CIS277: Introduction to Computer Graphics (Instructor: Professor <a href="http://www.cis.upenn.edu/~badler/">Norman Badler</a>).
+					<p><b>GPU Programming (Fall 2012) TA </b> |  Developed curriculum and assignments, 
+held office hours, graded assignments, etc. | Supervisor: <a href="http://www.seas.upenn.edu/~pcozzi/">Patrick Cozzi</a>
+					<br>	
+					<p><b>CIS277 Teaching Assistant (Spring 2012)</b> | Held office hours, graded assignments, etc. | Supervisor: <a href="http://www.cis.upenn.edu/~badler/">Norm Badler</a>
 					<br>
-					<p><b>CIS565 Teaching Assistant (Fall 2012)</b> | Created and graded assignments, held office hours, and taught selected lectures for the graduate level course CIS565: GPU Programming (Instructor: <a href="http://www.seas.upenn.edu/~pcozzi/">Patrick Cozzi</a>).
+					<div class="subtitle"><a href="http://www.dreamworksanimation.com/">Dreamworks Animation SKG, Inc.</a> | Glendale, CA | Jun 2012 - Aug 2012</div>
+					<p><b>Global Department Technical Director Intern</b> | Contributed to <b>tools and technologies</b> used by other departments to produce animated films. | Supervisor: <a href="https://www.linkedin.com/in/jontshih">Jon Shih</a>
 					<br>
-					<p><b>CIS563 Teaching Assistant (Spring 2013)</b> | Held office hours, assisted students, and graded assignments for the graduate level course CIS563: Physically Based Animation (Instructor: Professor <a href="http://www.seas.upenn.edu/~ladislav/">Ladislav Kavan</a>).
+					
+					<div class="subtitle"><a href="http://www.pixar.com/">Pixar Animation Studios</a> | Emeryville, CA | Jun 2011 - Aug 2011</div>
+					<p><b>Pixar Undergraduate Program Intern</b> | Completed weekly assignments in all <b>technical director</b> areas of Pixar’s <b>production pipeline</b>. | Supervisor: Fran Kalal
 					<br>
+					
 <!--
 					<div class="subtitle"><a href="http://www.venmo.com/">Venmo Inc.</a> | Philadelphia, PA | Sept. 2010 - Dec. 2010</div>
 					<p><b>Graphic Design/Software Development Intern</b> | Worked on the <a href="http://www.givegifi.com/">Gifi iPhone App</a> team as a designer and created a website and various visuals for the Gifi/Venmo marketing campaign.			
@@ -61,8 +82,39 @@
 				</div>
 			</div>
 			
+			<!-- COURSEWORK SECTION -->
+			
+			<!-- 
+			<div class = "resumerow">
+				<div class="resumeleftbar">
+					<div class="resumeheader">RELEVANT COURSEWORK</div>
+				</div>
+				<div class="resumerightbar">
+					<div class="resumerightbarsplitleft">
+						Algorithms and Data Structures (CS)<p>
+						Programming Languages and Techniques I & II (CS)<p>
+						Computer Architecture (CS)<p>
+						Intro to Computer Graphics (CS)<p>
+						Computer Graphics (CS)<p>
+						Automata, Computability, and Complexity (CS)<p>
+						
+					</div>
+					<div class="resumerightbarsplitright">
+						Physically Based Animation (CS)<p>
+						Mathematical Foundations of Computer Science (CS)<p>
+						GPU Programming	(CS)<p>
+						Digital Design Foundations (Art)<p>
+						Drawing (Art)<p>
+						3D Computer Modeling (Art)<p>
+						Computer Animation (Art)<p>
+					</div>
+				</div>
+			</div>
+			-->
+			
 			<!-- ACTIVITIES SECTION -->
 			
+			<!-- 
 			<div class = "resumerow">
 				<div class="resumeleftbar">
 					<div class="resumeheader">RELEVANT ACTIVITIES</div>
@@ -85,7 +137,21 @@
 		
 				</div>
 			</div>
+			-->
 			
+			<!-- PUBLICATIONS SECTION -->
+			
+			<div class = "resumerow">
+				<div class="resumeleftbar">
+					<div class="resumeheader">PUBLICATIONS</div>
+				</div>
+				<div class="resumerightbar">
+					<div class="subtitle">A Framework for the Experimental Comparison of Solar and Skydome Illumination | SIGGRAPH Asia 2014 | Dec 2014 </div>
+					<p>Authors: <a href="http://www.graphics.cornell.edu/~kiderj/">Joseph T. Kider</a>, <a href="http://www.danknowlton.com">Daniel T. Knowlton</a>, <a href="http://www.jeremynewlin.info/">Jeremy Newlin</a>, <a href="http://www.yiningkarlli.com/">Yining Karl Li</a>, <a href="http://www.graphics.cornell.edu/people/director.html">Donald P. Greenberg</a>
+		
+				</div>
+			</div>
+
 			<!-- SKILLS SECTION -->
 			
 			<div class = "resumerow">
@@ -94,38 +160,36 @@
 				</div>
 				<div class="resumerightbar">
 					<div class="resumerightbarsplitleft">
-						<div class="subtitle">Progamming</div><p>
+						<div class="subtitle">Programming</div><p>
 						C/C++ <p>
 						CUDA <p>
 						OpenGL/GLSL <p>
 						Python <p>
-						Java <p>
-						PHP/MySQL <p>
-						QT <P>
-						MEL <p>
+						Go <p>
 						Bash <p>
-						HTML5/CSS3/Javascript <p>
+						Mel <p>
+						Java <p>
 					</div>
 					<div class="resumerightbarsplitright">
 						<div class="subtitle">Graphics</div><p>
-						Renderman/Slim/Alfred (Pixar)<p>
-						Vray (Chaos Group)<p>
-						Mentalray (NVIDIA)<p>
-						Maya (Autodesk)<p>
-						Houdini (SideFX)<p>
-						Mudbox (Autodesk)<p>
-						Photoshop (Adobe)<p>
-						After Effects (Adobe)<p>
-						Premiere Pro (Adobe)<p>
-						InDesign (Adobe)<p>
-						Flash Professional (Adobe)<p>
-						Illustrator (Adobe)<p>
-						Lightroom (Adobe)<p>
-						Final Cut Pro (Apple)<p>
-						Aperture (Apple)<p>
+						Photorealistic Renderman<p>
+						Maya<p>
+						3Ds Max<p>
+						Photoshop <p>
+						Mudbox <p>
+						Arnold <p>
+						Mentalray <p>
+						After Effects <p>
+						Mari <p>
+						Houdini <p>
+						VRay <p>
+						Nuke <p>
 					</div>
 				</div>
 			</div>
+			<?php
+				include '../includes/footer.php';
+			?>
 		</div>
 	</body>
 </html>
